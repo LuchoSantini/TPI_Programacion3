1399e99bb56698defe93ee26e9762f3a9ab7b7f0
LuchoSantini <lucho_santini7@outlook.es>
2023-06-07 14:58:03 -0300

    tpi

---
+++ TPI_Prog3/ApiTPI/Models/DTO/ProductoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTO
+{
+    public class ProductoDTO
+    {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public int Color { get; set;}
+        public int Talle { get; set; }
+    }
+}
